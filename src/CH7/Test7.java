b2cf39bacaacc83be75c7c7f8ffb7ae9d9ca1ffc
chuchu1313 <fangyu1313@gmail.com>
2017-11-13 12:00:01 +0800

    Modify Test7.java

--- src/CH7/Test7.java
+++ src/CH7/Test7.java
@@ -4,7 +4,7 @@ package CH7;
 Which collection class(es) allows you to grow or shrink its size and provides indexed access to its elements,
 but whose methods are not synchronized? (Choose all that apply.)
 A. java.util.HashSet // Wrong! grow or shrink its size (ok) provides indexed access (no) not synchronized (ok)
-B. java.util.LinkedHashSet // Wrong! grow or shrink its size (ok) provides indexed access (ok) not synchronized (ok)
+B. java.util.LinkedHashSet // Wrong! grow or shrink its size (ok) provides indexed access (no) not synchronized (ok)
 C. java.util.List // Wrong! List is an interface
 D. java.util.ArrayList // Correct! grow or shrink its size (ok) provides indexed access (ok)  not synchronized (ok)
 E. java.util.Vector // Wrong! grow or shrink its size (ok) provides indexed access (ok) not synchronized (no)
